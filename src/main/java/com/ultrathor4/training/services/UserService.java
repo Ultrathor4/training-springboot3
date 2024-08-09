8ee14b2b4493e1d128ed7ab84f92fb177c80f1ee
Ultrathor4 <danilo.gomes.torquato@gmail.com>
2024-08-09 17:05:55 -0300

    Exception handling - update

--- src/main/java/com/ultrathor4/training/services/UserService.java
+++ src/main/java/com/ultrathor4/training/services/UserService.java
@@ -13,6 +13,8 @@ import com.ultrathor4.training.repositories.UserRepository;
 import com.ultrathor4.training.services.exceptions.DatabaseExcetion;
 import com.ultrathor4.training.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 
@@ -46,9 +48,14 @@ public class UserService {
 	}
 	
 	public User update(Long id, User obj) {
-		User entity = repository.getReferenceById(id);
-		updateData(entity, obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		}catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
+		
 	}
 
 	private void updateData(User entity, User obj) {
